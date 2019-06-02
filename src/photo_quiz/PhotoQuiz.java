1475b01ded99a93b52c4af8a80b9c52b9940d273
League Student <league@iMac-47.attlocal.net>
2019-06-02 11:25:41 -0700

    Level1-Module0 || 2 June 2019
    :neutral_face:

--- src/photo_quiz/PhotoQuiz.java
+++ src/photo_quiz/PhotoQuiz.java
@@ -5,7 +5,6 @@ package photo_quiz;
  */
 
 import java.awt.Component;
-import java.awt.Dimension;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -17,28 +16,29 @@ import javax.swing.JOptionPane;
 
 public class PhotoQuiz {
 
-	private JLabel Lilly;
-	private JLabel Sunflower;
-
 	public void run() throws Exception {
 
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
 		// This will make sure the program exits when you close the window
 		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		quizWindow.setPreferredSize(new Dimension(333,500));
+
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-		showLily();
+		String URL= "https://s3.amazonaws.com/cdn.brecks.com/images/500/73689A.jpg";
 		// 2. create a variable of type "Component" that will hold your image
+		Component Lily;
 		// 3. use the "createImage()" method below to initialize your Component
+		Lily = createImage(URL);
 		// 4. add the image to the quiz window
+		quizWindow.add(Lily);
 		// 5. call the pack() method on the quiz window
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-		String answer1 = JOptionPane.showInputDialog("What kind of flower is this?");
+		String answer = JOptionPane.showInputDialog("What type of flower is this?");
 		// 7. print "CORRECT" if the user gave the right answer
-		if (answer1.equalsIgnoreCase("lily")) {
+		if (answer.equalsIgnoreCase("Lily")) {
 			System.out.println("CORRECT");
 		}
 		// 8. print "INCORRECT" if the answer is wrong
@@ -47,53 +47,33 @@ public class PhotoQuiz {
 		}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-		quizWindow.remove(Lilly);
+		quizWindow.remove(Lily);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-		showSunflower();
-		// 11. add the second image to the quiz window
-
+		String URL2= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBk30X3alvdsHHRMHoSwVaN0IDgf_dgIUUikKJONAdx4Y3VrYDfw";
+		Component Daisy;
+		Daisy = createImage(URL2);
+		quizWindow.add(Daisy);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-		
+		String answer2 = JOptionPane.showInputDialog("What type of flower is this?");
 		// 14+ check answer, say if correct or incorrect, etc.
-		String answer2 = JOptionPane.showInputDialog("What kind of flower is this?");
-		if (answer2.equalsIgnoreCase("sunflower")) {
+		if (answer2.equalsIgnoreCase("Daisy")){
 			System.out.println("CORRECT");
 		}
 		else {
 			System.out.println("INCORRECT");
 		}
 	}
-
+	
 	private Component createImage(String icon2) throws MalformedURLException {
 		URL url = new URL(icon2);
 		Icon icon = new ImageIcon(url);
 		JLabel imageLabel = new JLabel(icon);
 		return imageLabel;
 	}
-	
-	private void showLily() {
-		JFrame quizWindow = new JFrame();
-		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		quizWindow.setVisible(true);
 
-		Icon icon = new ImageIcon(getClass().getResource("Lilly.jpg"));
-		this.Lilly = new JLabel(icon);
-		quizWindow.add(Lilly);
-		quizWindow.pack();
-	}
-	private void showSunflower() {
-		JFrame quizWindow = new JFrame();
-		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		quizWindow.setVisible(true);
-
-		Icon icon = new ImageIcon(getClass().getResource("Sunflower.jpg"));
-		this.Sunflower = new JLabel(icon);
-		quizWindow.add(Sunflower);
-		quizWindow.pack();
-	}
 	/* OPTIONAL */
 	// *14. add scoring to your quiz
 	// *15. make something happen when mouse enters image
